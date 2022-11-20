1945059ee2f62f349397b92e790642c986816c32
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-20 10:45:01 -0800

    Create a state wirandomized numbers array and render components with map

--- src/App.js
+++ src/App.js
@@ -4,33 +4,33 @@ import Die from "./Components/Die"
 
 function App() {
 
-  function allDice () {
+  const [ dice, setDice ] = React.useState( () => { 
+    return allNewDice()
+    })
+
+  function allNewDice () {
     let newArr = []
     for(let i=0;i<10;i++){
       let num = Math.ceil(Math.random()*6)
       newArr.push(num)
     }
-    console.log(newArr)
+    return newArr
   }
 
-  React.useEffect(() => {
-    allDice()
-  }, [])
+
+  const diceMaker = dice.map(diceElement => {
+    return (
+      <Die value={diceElement} />
+    )
+  })
+
+
 
   return (
     <main>
       <div className="box-container">
         <div className="inner-box">
-          <Die value="1"/> 
-          <Die value="2"/>
-          <Die value="3"/> 
-          <Die value="4"/>
-          <Die value="5"/> 
-          <Die value="6"/>
-          <Die value="7"/> 
-          <Die value="8"/>
-          <Die value="9"/> 
-          <Die value="10"/>
+          {diceMaker}
         </div>
       </div>
     </main>
